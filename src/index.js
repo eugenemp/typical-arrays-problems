82853c8499805c6d9db6165bd8fbe6c365f53d18
eugenemp <phoenixlp91@mail.ru>
2021-02-23 15:18:13 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  //Check if array is empty or no params specified 
+  //and use Math.min() method to find minimal value of array
+  return (!array || array.length === 0) ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  //Check if array is empty or no params specified 
+  //and use Math.max() method to find maximal value of array
+  return (!array || array.length === 0) ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  //Check if array is empty or no params specified 
+  //and use Array.reduce() result devided by array length to find an average value
+  return (!array || array.length === 0) ? 0 : 
+    array.reduce( (previous, current) => previous + current, 0 ) / array.length;
 }
